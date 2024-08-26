2dbeacc961ef19cfd45940b04f50ad35a411454e
Andrii Chubenko <stalkerock@gmail.com>
2024-08-26 14:25:35 +0300

    sol

--- musician/urls.py
+++ musician/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = []
-
 app_name = "musician"
+
+from django.urls import path
+from .views import MusicianListCreate, MusicianDetail
+
+urlpatterns = [
+    path('musicians/', MusicianListCreate.as_view(), name='manage-list'),
+    path('musicians/<int:pk>/', MusicianDetail.as_view(), name='musician-detail'),
+]
---
+++ musician/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from .models import Musician
+from .serializers import MusicianSerializer
+
+class MusicianListCreate(generics.ListCreateAPIView):
+    queryset = Musician.objects.all()
+    serializer_class = MusicianSerializer
+
+class MusicianDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Musician.objects.all()
+    serializer_class = MusicianSerializer
